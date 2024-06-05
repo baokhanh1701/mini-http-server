f9c399115208fff94aa13a9a39eb20a356a5381d
baokhanh1701 <89294586+baokhanh1701@users.noreply.github.com>
2024-06-05 23:21:52 +0700

    feat: extract url path

--- app/main.ts
+++ app/main.ts
@@ -1,15 +1,29 @@
-import * as net from 'net';
+import * as net from "net";
 
-const server = net.createServer((socket : any) => {
-    socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
-    socket.end();
+const server = net.createServer((socket: any) => {
+  // socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
+  socket.on("data", (data: any) => {
+    try {
+      console.log(data.toString());
+      socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
+      socket.end();
+    } catch (error) {
+      var string = data.toString();
+      console.log(string);
+    };
+    socket.on("error", (error : any) => {
+        console.log(error);
+        socket.write(`HTTP/1.1 404 Not Found\r\n\r\n`);
+        socket.end();
+    });
+  });
+  socket.end();
 });
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
-server.listen(4221, 'localhost', () => {
-    console.log('Server is running on port 4221');
-    
+server.listen(4221, "localhost", () => {
+  console.log("Server is running on port 4221");
 });
