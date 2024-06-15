261ac1e9efb62f3f12d1e5dada686e4fd09162f5
baokhanh1701 <89294586+baokhanh1701@users.noreply.github.com>
2024-06-15 14:39:04 +0700

    codecrafter: debug

--- app/main.ts
+++ app/main.ts
@@ -134,4 +134,5 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 server.listen(4221, "localhost", () => {
   console.log("Server is running on port 4221");
+  console.log("yeah still going...")
 });
