0f2c26e94820cb4de8f8ad1c1a684cd64e2137ba
baokhanh1701 <89294586+baokhanh1701@users.noreply.github.com>
2024-06-15 14:08:56 +0700

    codecrafter: debug

--- app/main.ts
+++ app/main.ts
@@ -56,7 +56,8 @@ const server = net.createServer((socket: any) => {
       const userAgent = req.split("\r\n")[2].split(" ")[1];
       const method = req.split(" ")[0];
       const content = req.split("\r\n")[req.split("\r\n").length - 1];
-      const compression = req.split("\r\n")[2].split(":")[1];
+      const compression = req.split("\r\n")[2].split(":")[1].split(", ");
+      
       console.log("REQUEST: ", req);
       console.log("-- path: ", path);
       console.log("-- User Agent: ", userAgent);
