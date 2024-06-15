c0480ddf8a0cf8506ad38c0b3cc79a11698b1c76
baokhanh1701 <89294586+baokhanh1701@users.noreply.github.com>
2024-06-15 18:08:02 +0700

    codecrafter: debug

--- app/main.ts
+++ app/main.ts
@@ -111,6 +111,7 @@ const server = net.createServer((socket: any) => {
           console.log("gzipped: ", gzipped);
           socket.write(`HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: ${query.length}\r\n\r\n`);
           socket.write(gzipped);
+          socket.end();
         } else {
           console.log("gzip header not supported, processing...");
           res = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${query.length}\r\n\r\n${query}`;
