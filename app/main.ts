3e5794bf61afb00fb709e4ccbfd23c71b636964c
baokhanh1701 <89294586+baokhanh1701@users.noreply.github.com>
2024-06-15 17:42:45 +0700

    codecrafter: debug

--- app/main.ts
+++ app/main.ts
@@ -106,11 +106,7 @@ const server = net.createServer((socket: any) => {
         if (compression && compression.includes("gzip")) {
           console.log("gzip header supported, processing...");
           const buffer = Buffer.from(query, "utf8");
-          const gzipped = zlib.gzip(buffer, (err : any, buffer : any) => {
-            if (err) {
-              console.error('An error occurred:', err);
-            }
-            console.log(buffer.toString())};
+          const gzipped = zlib.gzip(buffer)
           res = `HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: ${query.length}\r\n\r\n${gzipped}`;
         } else {
           console.log("gzip header not supported, processing...");
